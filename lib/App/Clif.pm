4f12e2060bf251145d3e88906cdf66dd499660b9
z448 <zed448@icloud.com>
2016-03-18 18:05:16 +0100

    tmp fix for malformd json

--- lib/App/Clif.pm
+++ lib/App/Clif.pm
@@ -9,27 +9,27 @@ BEGIN {
     require Exporter;
     our $VERSION = 0.01;
     our @ISA = qw(Exporter);
-    our @EXPORT = qw( clif );
+    our @EXPORT_OK = qw( clif );
 }
-
 
 
 my $cli = sub {
         my $this = shift;
         my( $curl, @cmd )= ();
-    while( <DATA> ){
-        open( my $o, "-|", "curl -s $_");
-        while( <$o> ){ $curl = $_ }
-        open( my $oO, "-|", q'curl -sL ' . $curl . q'.com/commands/using/'."$this".q'/json' );
-        my $j = <$oO>;
-        my $p = decode_json $j;
 
-        for my $p( @$p ){
-            my %cmd;
-            $cmd{$this} = $p;
-            push @cmd, { %cmd };
-        }; return \@cmd;
-    }
+        while( <DATA> ){
+            open( my $o, "-|", "curl -sk $_");
+                while( <$o> ){ $curl = $_ }
+                open( my $oO, "-|", q|curl -skL | . $curl . $this . q|/json| );
+                my $j = <$oO>;
+                my $p = decode_json $j;
+
+                for my $p( @$p ){
+                    my %cmd;
+                    $cmd{$this} = $p;
+                    push @cmd, { %cmd };
+                }; return \@cmd;
+        }
 };
 
 
@@ -43,4 +43,4 @@ sub clif {
 
 #source
 __DATA__
-https://gist.githubusercontent.com/z448/3dcb01b979a3b31b7716/raw/17de137a16f80d41c6ff13654550f38504cdd8ce/
+https://gist.githubusercontent.com/z448/779015ddc421f9f61c6a/raw/5681145b46f6c4510c640c8969430f12bc779435/
